f8cd9acaf4ba17b95b772793bb0420dd1fb53a01
buddhika4u <buddhika4u98@gmail.com>
2024-02-06 08:45:55 +0530

    Implement book store crud operations

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,29 @@
-import express  from "express";
-import { PORT } from "./config.js";
+import express from "express";
+import { PORT, MONGODB_URL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
 
 const app = express();
 
-app.get('/',(req,res) => {
-    console.log(req);
-    return res.status(234).send("Welcome");
+// Middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome");
 });
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
-});
+app.use("/books", booksRoute);
 
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("Database Connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
