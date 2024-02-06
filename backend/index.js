7c98999bbd6e4cfe41d979b73f13bb0ed09793b5
buddhika4u <buddhika4u98@gmail.com>
2024-02-06 10:19:02 +0530

    Frontend initial commit

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,25 @@ import { PORT, MONGODB_URL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
-// Middleware
+// Middleware for parsing requirest body
 app.use(express.json());
+
+// Middleware for handling cors policy
+// option 1 : Allow all origins with default of cors(*)
+// app.use(cors());
+
+// option 2 : Allow custom origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   console.log(req);
